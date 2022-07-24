eca0022fc4455e85ed5f072edcc64abf4a4d279b
Walessonrdreis <wallllase@hotmail.com>
2022-07-23 21:24:28 -0300

    Inserindo class BeerDTOBuilder

--- beer_api_digital_innovation_one/src/test/java/one/digitalinnovation/beerstock/BeerstockApplicationTests.java
+++ beer_api_digital_innovation_one/src/test/java/one/digitalinnovation/beerstock/BeerstockApplicationTests.java
@@ -3,8 +3,11 @@ package one.digitalinnovation.beerstock;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class BeerstockApplicationTests {
 
-
+	@Test
+	void contextLoads(){
+	}
 
 }
